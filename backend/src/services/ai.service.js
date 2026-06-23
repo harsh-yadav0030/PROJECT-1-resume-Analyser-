1cdb6cd685ebbe26f9374db10c99459aa743ab30
harsh <jarnailsingh9817@gmail.com>
2026-06-23 15:26:31 +0530

    done

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -170,17 +170,33 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
                         The content should be ATS friendly, i.e. it should be easily parsable by ATS systems without losing important information.
                         The resume should not be so lengthy, it should ideally be 1-2 pages long when converted to PDF. Focus on quality rather than quantity and make sure to include all the relevant information that can increase the candidate's chances of getting an interview call for the given job description.`;
 
-    const response = await ai.models.generateContent({
-      model: "gemini-3-flash-preview",
-      contents:prompt,
-      config: {
-        responseMimeType: "application/json",
-        responseSchema: zodToJsonSchema(resumePdfSchema),
-      }
-    });
-    const jsonContent = JSON.parse(response.text);
-    const pdfBuffer = await generatePdfFromHtml(jsonContent.html)
-    return pdfBuffer
-}
+    const response = await generateWithRetry(() =>
+        ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: prompt,
+            config: {
+                responseMimeType: "application/json",
+                responseSchema: zodToJsonSchema(resumePdfSchema),
+            }
+        })
+    );
+  };
+
+
+async function generateWithRetry(fn, retries = 3) {
+    for (let i = 0; i < retries; i++) {
+        try {
+            return await fn();
+        } catch (err) {
+            if (err.status !== 503 || i === retries - 1) {
+                throw err;
+            }
+
+            await new Promise(resolve =>
+                setTimeout(resolve, 2000 * (i + 1))
+            );
+        }
+    }
+} 
 
 export { generateInterviewReport  , generateResumePdf };
